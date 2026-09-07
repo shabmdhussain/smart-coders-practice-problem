6f774c4835e7a59d49ba91c4af312d22450126f5
shabmdhussain <shabmdhussain@gmail.com>
2026-09-07 15:15:13 +0530

    Add files via upload

---
+++ problem1.py
@@ -0,0 +1,7 @@
+num1=int(input("enter the first number"))
+num2=int(input("enter the second number"))
+print(f"sum:{num1+num2}")
+print(f"product:{num1*num2}")
+print(f"quotient:{num1/num2}")
+print(f"difference:{num1-num2}")
+print(f"remainder:{num1%num2}")
