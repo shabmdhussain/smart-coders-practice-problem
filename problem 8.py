28515be90f6f260c0445fe891be1f5a5a24fc049
shabmdhussain <shabmdhussain@gmail.com>
2026-09-08 14:28:39 +0530

    Add files via upload

---
+++ problem 8.py
@@ -0,0 +1,7 @@
+a=int(input("enter the first number:"))
+b=int(input("enter the second number:"))
+c=int(input("enter the third number:"))
+if a==b and b==c:
+    print(f"{a} {b} {c} are equal")
+else:
+    print("are not equal")   
