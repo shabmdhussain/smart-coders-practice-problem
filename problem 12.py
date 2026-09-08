4f93ddea17747a82cc4b59e221f06149ef9c07ec
shabmdhussain <shabmdhussain@gmail.com>
2026-09-08 15:23:24 +0530

    Add files via upload

---
+++ problem 12.py
@@ -0,0 +1,6 @@
+a=int(input("enter a number:"))
+b=int(input("enter a number:"))
+a,b=b,a
+print("after swapping:")
+print("a=",a)
+print("b=",b)
---
+++ problem 13.py
@@ -0,0 +1,3 @@
+n=int(input("enter the number:"))
+print("n>>1=",n>>1)
+print("n<<1=",n<<1)
