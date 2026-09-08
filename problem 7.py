49a1334acb214fd4efe6ae4b423dd0d047d576cd
shabmdhussain <shabmdhussain@gmail.com>
2026-09-08 14:16:48 +0530

    Add files via upload

---
+++ problem 7.py
@@ -0,0 +1,9 @@
+a=int(input("enter the number"))
+if a>0:
+    print(f"the number is positive")
+elif a<0:
+    print(f"the number is negative")
+elif a==0:
+    print(f"the number is zero")
+else:
+    print("invalid")            
