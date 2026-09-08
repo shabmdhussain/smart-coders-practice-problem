26b49633ac8a26c2e179ab66965675b04f31cbc7
shabmdhussain <shabmdhussain@gmail.com>
2026-09-08 11:58:28 +0530

    Add files via upload

---
+++ problem 5.py
@@ -0,0 +1,13 @@
+a=int(input("enter the number:"))
+if a%3==0 and a%5==0:
+    print(" the number is divisble by 3 and 5")
+elif a%3==0:
+    print("the number is divisble by 3")
+elif a%5==0:
+    print("the number is divisble by 5")
+else:
+    print("the number is not divisble by 3 and 5")
+        
+    
+    
+    
---
+++ problem 6.py
@@ -0,0 +1,8 @@
+num1=int(input("enter the number:"))
+num2=int(input("enter the number:"))
+if num1>num2:
+    print(f"the greater number is {num1}")
+elif num2>num1:
+    print(f"the greater number is {num2}")
+else:
+    print(" both are equal")       
