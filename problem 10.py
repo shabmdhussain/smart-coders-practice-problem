5a311dd06ef685f5a75c0641c17fbc3c94880c58
shabmdhussain <shabmdhussain@gmail.com>
2026-09-08 14:52:26 +0530

    Add files via upload

---
+++ problem 10.py
@@ -0,0 +1,20 @@
+ch=input("enter the character:")
+if ch>='A'and ch<='Z':
+    print("uppercase")
+elif ch>='a' and ch<='z':
+    print("lowercase")
+elif ch>='0' and ch<='9':
+    print("digit")
+else:
+    print("special character")        
+    
+      
+
+
+
+
+
+
+
+
+
