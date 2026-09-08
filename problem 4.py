81484286100d4b8619795e01c5c5ec75f8594118
shabmdhussain <shabmdhussain@gmail.com>
2026-09-08 11:11:12 +0530

    Add files via upload

---
+++ problem 4.py
@@ -0,0 +1,7 @@
+C=int(input("enter the temperature in celsius:"))
+f=(C*9/5)+32
+print(f"temperature in fahrenheit=",f)
+
+f=int(input("enter the temperature in fahrenheit:"))
+c=(f-32)*5/9
+print(f"enter the temperature in celsius=",c)
